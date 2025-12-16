d7760d0a589edb67700d7269d03e9983f9ba0e7a
73945 <jakubpara2007@gmail.com>
2025-12-16 13:26:28 +0100

    z laboratoriów

--- zad4.py
+++ zad4.py
@@ -1,3 +1,11 @@
 import numpy as np
 liczby=np.array( [128, 64, 32, 16, 8, 4, 2, 1])
-ndarray=[1,0,1,0,1,1,0,0]
+import random
+ndarray=np.random.randint(0,2,size=8)
+print(ndarray)
+
+def wartosc_liczby_bin(liczby, ndarray):
+    return np.sum(liczby * ndarray)
+
+print(wartosc_liczby_bin(liczby, ndarray))
+
