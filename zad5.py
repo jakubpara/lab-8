74faf3179f47df1d95a7202fff7a85038374766b
jakubpara <jakubpara2007@gmail.com>
2025-12-22 21:48:03 +0100

    zad5

--- zad5.py
+++ zad5.py
@@ -9,5 +9,10 @@ najmniejsza= random5x5.min()
 print(najmniejsza)
 
 #najwieksze/najmniejsze elementy w kazdej kolumnie i wierszu
-
+maksymalne_pion=np.max(random5x5,axis=0)
+print(maksymalne_pion)
+maksymalne_poziom=np.min(random5x5,axis=1)
+print(maksymalne_poziom)
 #suma wartości w wierszach
+suma=np.sum(random5x5,axis=1)
+print(suma)
